68625a99611ef5f4d3470f05e4e39d2f15114ce4
Vikas Sundrani <sundrani.vikas@gmail.com>
2016-11-17 18:30:12 -0500

    heroku commit

--- server.js
+++ server.js
@@ -1,39 +1,14 @@
-/* 6- Sequelize with Cats
- *  (((SOLUTION)))
- * -/-/-/-/-/-/-/-/-/-/-/ */
-
-
-/* STUDENTS
- * Create a database called cat_with_sequelize_db
- *
- * Your instructions lie in users_controller.js
- * and cats_controller.js
- * Good luck!
- * -/-/-/-/-/ */
-
-
-
 // Dependencies
 // ============
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser'); // for working with cookies
 var bodyParser = require('body-parser');
-var session = require('express-session'); 
 var methodOverride = require('method-override'); // for deletes in express
 
 
 // Our model controllers (rather than routes)
 var application_controller = require('./controllers/application_controller');
-
-var cats_controller = require('./controllers/cats_controller');
-
-var users_controller = require('./controllers/users_controller');
-
-
-
 
 // Express settings
 // ================
@@ -44,31 +19,15 @@ var app = express();
 // override POST to have DELETE and PUT
 app.use(methodOverride('_method'))
 
-//allow sessions
-app.use(session({ secret: 'app', cookie: { maxAge: 60000 }}));
-app.use(cookieParser());
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
-//set up handlebars
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
-
+/
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', application_controller);
-app.use('/cats', cats_controller);
-app.use('/users', users_controller);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
