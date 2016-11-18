1adb956d8f238386aa491851719bdfb358b663bb
Vikas Sundrani <sundrani.vikas@gmail.com>
2016-11-17 19:36:13 -0500

    video,quotes

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -22,3 +22,36 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+var vidArray = ['RYlCVwxoL_g', 'u2cMjeSvZSs', 'c1H92b_uLdU', 'eqhUHyVpAwE', '9vdN15--hro', 'xJ9e32MNEOk', 'xJ9e32MNEOk', 'g-jwWYX7Jlo', 'mgmVOuLgFB0', 'CPQ1budJRIQ', 'XNj_KDPp_iM', 'PyDlBy5tgYA']
+
+function onYouTubeIframeAPIReady() {
+
+	var currentVideo = vidArray[Math.floor(Math.random()*vidArray.length)];
+
+	var player = new YT.Player("player", {
+	                    height: '360',
+	                     width: '640',
+	                     videoId: currentVideo,	                     
+	               });
+
+        console.log("API Ready");
+
+}
+
+$(document).ready(function(){
+
+$.ajax({
+            url: "http://api.forismatic.com/api/1.0/",
+            jsonp: "jsonp",
+            dataType: "jsonp",
+            data: {
+              method: "getQuote",
+              lang: "en",
+              format: "jsonp"
+            }
+         }).then(function(response) {
+            $(".quote").empty().text(response.quoteText);
+        });
+
+})
